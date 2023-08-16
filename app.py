7b9c700d0c0d0d7f88971e280067cfbd90927d45
Prathamesh patil <prathmesh8485049898@gmail.com>
2023-08-16 17:47:22 +0530

    google sheet automation using python

--- app.py
+++ app.py
@@ -14,4 +14,28 @@ headers = values[0]
 
 df = pd.DataFrame(values[1:], columns=headers)
 num_rows = df.columns
-print("Number of rows:", num_rows)
+#print("Number of rows:", num_rows)
+data = []
+family = {} 
+
+for i in range(len(df)):
+    dfHead = df.iloc[i:i+1, range(15)]
+    dfHome = df.iloc[i:i+1, 15:27]
+    dfAnimals = df.iloc[i:i+1, 27:35]
+    dfGeneral = df.iloc[i:i+1, 35:50]
+    dfPersin1 = df.iloc[i:i+1, 50:65]
+    dfPerson2 = df.iloc[i:i+1, 65:80]
+    dfPerson3 = df.iloc[i:i+1, 80:95]
+    
+    family[df["कुटुंब प्रमुखाचे नावं "][i]] = {
+        'dfHead': dfHead,
+        'dfHome': dfHome,
+        'dfAnimals': dfAnimals,
+        'dfGeneral': dfGeneral,
+        'dfPersin1': dfPersin1,
+        'dfPerson2': dfPerson2,
+        'dfPerson3': dfPerson3
+    }
+
+data.append(family) 
+print(data)
